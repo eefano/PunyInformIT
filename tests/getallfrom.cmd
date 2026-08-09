7c69bbdb32329ed1cd99efea0c1d9aff89d42fc0
eefano <77832+eefano@users.noreply.github.com>
2026-08-10 00:09:31 +0200

    getallfrom test ok

---
+++ tests/getallfrom.cmd
@@ -0,0 +1,17 @@
+transcript
+
+prendi tutto tranne la scatola
+prendi tutto dalla borsa
+lascia tutto
+prendi tutti i sassi
+lascia tutti
+s
+prendi tutto tranne la scatola
+prendi tutto dalla borsa
+lascia tutto
+n
+n
+prendi tutto dalla borsa
+termina
+si
+
