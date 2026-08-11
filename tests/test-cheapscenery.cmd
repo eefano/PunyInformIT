c94db47fa5a6cea75bcb4ed90a070153f6544baa
eefano <77832+eefano@users.noreply.github.com>
2026-08-11 14:21:01 +0200

    test-cheapscenery ok

--- tests/test-cheapscenery.cmd
+++ tests/test-cheapscenery.cmd
@@ -6,7 +6,6 @@ prendi vecchio libro
 x giornale
 spingi giornali
 esamina -li
-esamina -lo
 x la grande libreria
 spingi la libreria
 x aria
@@ -16,14 +15,14 @@ tira muri
 x soffitto
 spingi soffitto
 x quello
-x pulsante d'emergenza
+x pulsante d' emergenza
 x luce
-spingi pulsante d'emergenza
-tira pulsante d'emergenza
+spingi pulsante d' emergenza
+tira pulsante d' emergenza
 accendi interruttore
 x luce
-spingi pulsante d'emergenza
-tira pulsante d'emergenza
+spingi pulsante d' emergenza
+tira pulsante d' emergenza
 e
 x quello
 x libro
