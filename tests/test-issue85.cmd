0727e32aec4d0025a93e3ba420afb27f781c99a9
eefano <77832+eefano@users.noreply.github.com>
2026-08-11 23:50:03 +0200

    test-issue85 ok

---
+++ tests/test-issue85.cmd
@@ -0,0 +1,16 @@
+transcript
+
+spingi mela nella scatola
+spingi banana nella scatola
+spingi limone nella scatola
+v
+i
+prendi banana
+svuota la scatola
+dai mela agli indiani
+dai mela a john
+chiedi agli indiani me
+chiedi a John me
+termina
+si
+
