bb40951d38cfa7a359aaa7f2a0138c5238f9e3ae
eefano <77832+eefano@users.noreply.github.com>
2026-08-11 15:09:29 +0200

    test-grammarproperty ok

---
+++ tests/test-grammarproperty.cmd
@@ -0,0 +1,15 @@
+transcript
+
+john, prendi la mira
+john, prendi prendi la mira
+john, ciao
+john, prendi me
+john, salta
+john, apri mela
+john, chiudi mela
+john, spegni mela
+john, mangia mela
+john, bevi mela
+termina
+si
+
