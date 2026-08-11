a24dd553a3a979fded67adcba833dfe5bf663790
eefano <77832+eefano@users.noreply.github.com>
2026-08-11 15:17:39 +0200

    test-inputaction ok

---
+++ tests/test-inputaction.cmd
@@ -0,0 +1,24 @@
+transcript
+
+prendi tutto
+lascia tutto
+prendi libro
+prendi palla
+i alto
+lascia tutto
+mangia libro
+i
+lascia tutto
+x palla
+mangia palla
+metti palla nella scatola
+prendi palla
+lascia palla
+prendi -la
+i
+lascia tutto
+metti libro in scatola
+i
+termina
+si
+
