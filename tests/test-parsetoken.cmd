a3c892a95ecbd16aa5ad3e848ad3beaf4cb1c29b
eefano <77832+eefano@users.noreply.github.com>
2026-08-12 01:22:16 +0200

    test-parsetoken ok

---
+++ tests/test-parsetoken.cmd
@@ -0,0 +1,49 @@
+transcript
+
+.
+..
+,
+
+x john
+x bello
+x il bello john
+scan1 carta
+scan1 carta e cartapecora
+scan1 carta con me
+scan1 tutto
+scan2 carta
+scan2 carta e cartapecora
+scan2 tutto
+scan3 carta
+scan3 carta qui
+scan3 john qui
+scan3 john
+scan4 carta
+scan4 carta qui
+scan4 john qui
+scan4 john
+spara 100
+sbalordisci per asdsadas
+sbalordisci per john
+sbalordisci per mela
+spara xyzzy
+yeah
+s
+x uovo
+x tuorlo
+x succulento
+x succulento uovo
+x succulento tuorlo
+x l' uovo
+x il succulento uovo
+x mela
+x albicocca
+x banana
+prendi albicocca e uovo
+prendi banana e uovo
+prendi mela. vai nord
+s
+prendi banana. vai nord
+termina
+si
+
