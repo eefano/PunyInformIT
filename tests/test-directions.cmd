b98efcb52c5a6179e1ebbd8ea1d3bd458b698bca
eefano <77832+eefano@users.noreply.github.com>
2026-08-11 13:39:10 +0200

    test-directions ok

---
+++ tests/test-directions.cmd
@@ -0,0 +1,15 @@
+transcript
+
+x statua nord
+x statua
+nord
+mira statua nord
+mira a est della statua nord
+mira a nord
+prendi tutto tranne e
+prendi s e e
+spingi e o
+sblocca ovest con su
+termina
+si
+
