8eb8f8cbf257f8e11f1da208119970cb63dd8f6f
eefano <77832+eefano@users.noreply.github.com>
2026-08-12 10:06:03 +0200

    test-score ok

---
+++ tests/test-score.cmd
@@ -0,0 +1,17 @@
+transcript
+
+prendi punto
+lascia punto
+prendi tutto
+apri scatola
+metti tutto in scatola
+ricomincia
+si
+prendi punto
+apri scatola
+metti punto nella scatola
+prendi veleno
+mangia veleno
+termina
+si
+
