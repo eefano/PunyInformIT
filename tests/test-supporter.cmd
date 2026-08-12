e7a89d9b659f9a3fa29cbe243a1fed05a89f014e
eefano <77832+eefano@users.noreply.github.com>
2026-08-12 10:16:48 +0200

    test-supporter ok

---
+++ tests/test-supporter.cmd
@@ -0,0 +1,18 @@
+transcript
+
+guarda
+prendi vassoio
+i
+i alto
+metti vassoio nella giara
+prendi giara
+metti vassoio nella giara
+apri giara
+metti vassoio in giara
+i
+i largo
+prendi cucchiaio
+i
+termina
+si
+
