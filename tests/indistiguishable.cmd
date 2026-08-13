d49740847e61d9239509717b30b0dbadd677ce3b
eefano <77832+eefano@users.noreply.github.com>
2026-08-13 13:06:46 +0200

    indistinguishable ok

---
+++ tests/indistiguishable.cmd
@@ -0,0 +1,51 @@
+transcript
+
+guarda
+lascia due dadi
+prendi un dado
+prendi un peso da 5 chili
+lascia tutto
+prendi due oro
+i
+lascia tutto
+prendi due monete
+i
+lascia tutto
+guarda
+x moneta
+argento
+x monete
+oro
+prendi argento
+prendi monete d' argento
+prendi moneta d' oro
+prendi monete
+x dado
+x dadi
+cerca dado
+cerca dadi
+prendi dadi
+lancia dado al porta
+guarda
+lancia dado al porta
+guarda
+lancia dado al porta
+guarda
+prendi tutti i dadi
+guarda
+metti tutto nella scatola
+chiudi scatola
+lascia 5 monete
+prendi 5 monete
+prendi monete
+s
+spingi 2
+spingi due
+spingi secondo bottone
+s
+x 1
+x 2
+termina
+si
+
+
