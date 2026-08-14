92e82acc48204cd3d7b044cbb41393d07a70847a
eefano <77832+eefano@users.noreply.github.com>
2026-08-14 14:18:58 +0200

    test-parser ok! Wow.

---
+++ tests/test-parser.cmd
@@ -0,0 +1,108 @@
+
+transcript
+
+john, prendi torcia e felpa
+dici test
+dici test a john
+offri porta con torcia
+offri felpa con la scala
+offri felpa con la scala
+lascia tutto
+guarda nord
+x nord
+apri
+siedi scala
+guarda verso la scala
+guarda scala
+ancora me
+n
+ancora
+ancora me
+metti felpa in o
+svuota felpa
+lascia felpa
+metti scala su scala
+metti scala in scala
+john, prendi tutto
+vai n
+vai felpa
+vai
+prendi
+x sfera
+smanaccia -la
+x scala
+smanaccia scala
+smanaccia -la
+pronouns
+prendi scala e
+bacia scala
+apri -la
+digita abc su scala
+digita abc nella scala
+chiudi chiudi porta
+apri chiudi porta
+i.i
+apri torcia e la scala
+i blu
+apri tutto
+metti tutto
+prendi torcia
+metti tutto
+prendi torcia e john
+apri scatola
+prendi tutto da scatola
+i
+metti tutto
+metti tutto eccetto
+metti tutto eccetto in scatola
+metti tutto eccetto libro nella scatola
+prendi tutto dalla scatola
+metti tutto eccetto libro rosso nella scatola
+i
+lascia tutto
+prendi libro rosso, xyzzy e torcia
+prendi xyzzy e torcia
+prendi xyzzy
+x verde
+prendi verde
+leggi verde
+purloin libro rosa
+i
+prendi libro
+lascia rosa
+prendi tutto
+spingi dsdasdsa
+ops john
+r
+spingi john
+s
+lascia tutto
+prendi auto e est
+prendi blu e tutti i libri
+lascia tutto
+prendi libro blu e tutti i libri tranne i blu
+lascia tutto
+prendi libri
+lascia tutto
+prendi rosino
+lascia tutto
+prendi blu e blu e blu
+prendi blu e blu
+prendi blu
+prendi libro
+n
+prendi borsa
+metti felpa in borsa
+i
+i alto
+i largo
+lascia tutto
+offri
+offri john
+offri scala
+x torcia
+x torcia ora
+x torcia torcia ora
+termina
+si
+
