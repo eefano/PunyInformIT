43ed979a87878c6de8e94e5e559df28b2f8f7dfb
eefano <77832+eefano@users.noreply.github.com>
2026-08-14 14:16:42 +0200

    test per le direzioni

---
+++ tests/it-direzioni.cmd
@@ -0,0 +1,20 @@
+transcript
+
+guarda
+metti libro in n
+metti libro in s
+metti libro in e
+metti libro in o
+metti libro in ne
+metti libro in no
+metti libro in se
+metti libro in so
+metti libro in u
+metti libro in g
+metti libro in po
+metti libro in pr
+metti libro in tr
+metti libro in ba
+termina
+si
+
